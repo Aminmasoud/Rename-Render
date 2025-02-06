c7c71de5749243294f27044efcbead25608eb75f
Aminmasoud <85630921+Aminmasoud@users.noreply.github.com>
2025-02-06 08:49:04 +0330

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_ID = os.environ.get("API_ID", "12954268")
 
 API_HASH = os.environ.get("API_HASH", "9b9cf48eaea34bdf98d394d47b12f3e3")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8037609147:AAFP1zYE-za8j301pUTF6aFdIh9tBVPqgmsmongodb+srv://aminmasoudch:osDdPDg2zmGDpYtM@cluster0.npwmp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8037609147:AAFP1zYE-za8j301pUTF6aFdIh9tBVPqgms") 
 
 FORCE_SUB = os.environ.get("FORCE_SUB", "cinephilechannel") 
 
